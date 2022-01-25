2b0629256ebb824610c07ab44917405c3d0ca778
Saba <sabbamaghradze@gmail.com>
2022-01-25 19:42:21 +0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map, Observable, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'newtyping';
+  url = 'https://api.publicapis.org/entries';
+  sentences: any;
+  sentence: any;
+  initialLoad = true;
+  match = false;
+
+  value: string = '';
+
+  constructor(private http: HttpClient) {
+    http
+      .get(this.url)
+      .pipe(map((res: any) => res.entries))
+      .subscribe((data) => {
+        this.sentences = data
+          .slice(0, 100)
+          .map((article: any) => article.Description);
+        this.loadSentences();
+      });
+  }
+
+  compare(main: string, input: string) {
+    if (!input) return 'pending';
+
+    return main === input ? 'correct' : 'incorrect';
+  }
+
+  onChange(e: any) {
+    this.value = e.target.value;
+    if (this.sentence.join('') === this.value) this.match = true;
+    else this.match = false;
+  }
+
+  loadSentences() {
+    let n = Math.floor(Math.random() * 100 + 1);
+    this.initialLoad = false;
+    this.sentence = this.sentences[n].split('');
+    // console.log(this.sentence.split(''));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
